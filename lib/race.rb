47a9a59557b25cd9da598200191320b1b457e28e
Nikolay Dyulgerov <dyulgerov.nikolay@gmail.com>
2017-06-23 11:57:13 +0300

    Add scores for top five positions

--- lib/race.rb
+++ lib/race.rb
@@ -3,7 +3,15 @@ module RaceBet
 
     class << self
       def score(guesses, winners)
-
+        result = 0
+        guesses.each_with_index do |guess, position|
+          result += 15 if guess == winners[position] && position == 0
+          result += 10 if guess == winners[position] && position == 1
+          result += 5 if guess == winners[position] && position == 2
+          result += 3 if guess == winners[position] && position == 3
+          result += 1 if guess == winners[position] && position == 4
+        end
+        result
       end
     end
 
