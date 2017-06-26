5c6e3806599dec29f2e28afa721e51cf6ef1615f
Nikolay Dyulgerov <dyulgerov.nikolay@gmail.com>
2017-06-26 09:13:32 +0300

    Add points class variable

--- lib/race.rb
+++ lib/race.rb
@@ -1,13 +1,13 @@
 module RaceBet
   class Race
+    @@points = [ 15, 10, 5, 3, 1 ]
 
     class << self
       def score(guesses, winners)
         result = 0
-        points = [ 15, 10, 5, 3, 1 ]
         winners.each_with_index do |winner, place|
           if winner == guesses[place]
-            result += points[place]
+            result += @@points[place]
           elsif winners.include? guesses[place]
             result +=1
           end
